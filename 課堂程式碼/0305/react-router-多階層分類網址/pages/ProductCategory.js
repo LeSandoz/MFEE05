d129f24bfef2bf32deaa8b24bbefd2ae0bc1ba61
shinder <lsd0125@gmail.com>
2020-03-05 12:01:16 +0800

    Update ProductCategory.js

--- 課堂程式碼/0305/react-router-多階層分類網址/pages/ProductCategory.js
+++ 課堂程式碼/0305/react-router-多階層分類網址/pages/ProductCategory.js
@@ -5,20 +5,18 @@ import { Route, withRouter, NavLink, Switch, matchPath } from 'react-router-dom'
 import Product from './Product'
 
 function ProductCategory(props) {
-  // 利用match.params來取得url params (記得App元件中的Route要先定義)
+  // 利用match來取得url與path (記得App元件中的Route要先定義)
   console.log(props.match.pathname)
-
-  // 利用location.search來取得query string
-  //console.log('props.location.search', props.location.search)
-  // 利用內建的API來得到URLSearchParams物件 (注意IE完全不相容，要改用query-string模組)
-  //const searchParams = new URLSearchParams(props.location.search)
 
   const url = props.match.url
   const path = props.match.path
+  
+  // 有自己的選單與路由表
   return (
     <>
       <Banner pagename="產品分類" />
       <Breadcrumb />
+	  
       <nav class="nav nav-pills">
         <NavLink activeClassName="active" className="nav-link" to={`${url}/pc`}>
           PC

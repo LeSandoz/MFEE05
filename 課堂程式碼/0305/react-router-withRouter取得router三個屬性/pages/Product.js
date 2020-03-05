33cfce4ba76f7c47b15522303931ad79827f3bde
shinder <lsd0125@gmail.com>
2020-03-05 09:44:59 +0800

    add

---
+++ 課堂程式碼/0305/react-router-withRouter取得router三個屬性/pages/Product.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Banner'
+import Breadcrumb from '../components/Breadcrumb'
+
+function Product() {
+  return (
+    <>
+      <Banner pagename="產品" />
+      <Breadcrumb />
+    </>
+  )
+}
+
+export default Product

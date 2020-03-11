b36c7250a857fb3b2de68840077a1854c73522aa
shinder <lsd0125@gmail.com>
2020-03-11 13:50:27 +0800

    Add

---
+++ 課堂程式碼/0311/scss與bootstrap覆蓋應用/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+    </>
+  )
+}
+
+export default App

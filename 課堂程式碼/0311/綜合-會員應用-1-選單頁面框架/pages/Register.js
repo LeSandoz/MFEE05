e8a7a4bb4764d79ef90a81e14b1baded2900e31f
shinder <lsd0125@gmail.com>
2020-03-11 14:11:34 +0800

    ADD

---
+++ 課堂程式碼/0311/綜合-會員應用-1-選單頁面框架/pages/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../components/Banner'
+
+function Register() {
+  return (
+    <>
+      <Banner pagename="Register" />
+    </>
+  )
+}
+
+export default Register
